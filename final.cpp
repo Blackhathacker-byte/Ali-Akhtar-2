de6ff81adf031d8a6e3c65eb93f1c2fb807c0aa5
Blackhathacker-byte <aliboss60256@gmail.com>
2026-02-22 13:19:58 +0500

    message

---
+++ final.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+main(){
+
+cout<<"            ------------------------------------------------------- "<<endl;
+cout<<"            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    "<<endl;   
+cout<<"            -------------------------------------------------------    "<<endl;
+cout<<"                                                                          "<<endl;
+cout<<"                      ALI APARTMENT RENTAL ASSOCIATION                       "<<endl;
+cout<<"            _______________________________________________________                 "<<endl;
+cout<<"                                                                                     "<<endl;
+cout<<"            LOGIN●                                                                      "<<endl;
+cout<<"                    username:                                                             "<<endl;
+cout<<"                    password:                                                          "<<endl;
+cout<<"                    City    :                                                         "<<endl;                 
+cout<<"            _______________________________________________________                  "<<endl;
+cout<<"                                                                                      "<<endl;
+cout<<"            1.Check Apartments for Rents                                              "<<endl;
+cout<<"            2.Pay Apartment bill                                                      "<<endl;
+cout<<"            3.Apartment change request                                                "<<endl;
+cout<<"            4.Buy your dream Apartment                                                     "<<endl;
+cout<<"            5.Apartments on the Installment                                                 "<<endl;
+cout<<"                                                                                                  "<<endl;
+cout<<"                                                                                                "<<endl;
+cout<<"                                                                                               "<<endl;
+cout<<"                  FILE A QUERY OR COMPLAINT: 0300-0000000                                   "<<endl;
+cout<<"                                             0300-0000000                                     "<<endl;
+}
