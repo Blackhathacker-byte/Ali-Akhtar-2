829f15bf64cb7165d548525643e2b97683cfd1a4
ALI AKHTAR <aliboss60256@gmail.com>
2026-03-01 20:03:43 +0500

    message

---
+++ octa.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main(){
+         
+
+ cout<<"                                                                                   "<<endl;
+ cout<<"                                 =========== LUDO GAME BOARD ===========           "<<endl;
+ cout<<"                                                                                   "<<endl;
+ cout<<"                                 +-------------------------------------+            "<<endl;
+ cout<<"                                 |            |-----|-----|            |            "<<endl;
+ cout<<"                                 |     R      |     |     |     G      |            "<<endl;
+ cout<<"                                 |            |     |     |            |            "<<endl;
+ cout<<"                                 |------------+-----------+------------|            "<<endl;
+ cout<<"                                 |_|__________|    WIN    |__________|_|            "<<endl;
+ cout<<"                                 | |          |           |          | |            "<<endl;
+ cout<<"                                 |------------+-----------+------------|            "<<endl;
+ cout<<"                                 |            |     |     |            |            "<<endl;
+ cout<<"                                 |     B      |     |     |     Y      |            "<<endl;
+ cout<<"                                 |            |-----|-----|            |            "<<endl;         
+ cout<<"                                 +-------------------------------------+            "<<endl;
+ cout<<"                                                                                    "<<endl;
+ cout<<"                                      R = Red Player, G = Green Player              "<<endl;
+ cout<<"                                      B = Blue Player, Y = Yellow Player            "<<endl;
+ cout<<"                                                                                    "<<endl;
+
+}
